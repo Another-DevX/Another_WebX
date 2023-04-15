6827c988be5c1311f2e8b9b6801d1a7afa242278
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-04-14 23:51:14 -0500

    First commit This is a first deploy of a vite-react_ts app, i've added sass too

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='global__container'>
+      <h1>This is my personal website</h1>
+      <p>Would you like to travel in the sideral space with me?</p>
+    </main>
+  )
+}
+
+export default App
